b8578220eed8cd1bcca10c4e32827d3a3a68d14d
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-07-14 15:29:26 +0530

    Cart Functionality completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,7 @@ import store from './store.js'
 import App from './App.jsx'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
+import CartScreen from './screens/CartScreen.jsx'
 import './assets/styles/index.css'
 import './assets/styles/bootstrap.custom.css'
 // import "bootswatch/dist/darkly/bootstrap.min.css"
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 )
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './slices/apiSlice'
+import cartSliceReducer from './slices/cartSlice'
 
 const store = configureStore({
-    reducer : {
-        [apiSlice.reducerPath] : apiSlice.reducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: import.meta.env.NODE_ENV !== 'production' ? true : false
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: import.meta.env.NODE_ENV !== 'production' ? true : false,
 })
 
 export default store
