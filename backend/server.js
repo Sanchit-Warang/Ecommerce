82b5a894688da043aacc9ad8b86c58ad71f23a96
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-07-19 01:20:18 +0530

    backend auth done

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import path from 'path'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 const port = process.env.PORT || 5000
 
 connectDB() // connect to MongoDB
 
 const app = express()
 
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//Cookie Parser middleware
+app.use(cookieParser())
+
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve()
